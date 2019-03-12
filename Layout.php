123131c250460f3544bc23e2d8005deba4a02002
Troy Hurteau <jthurtea@ncsu.edu>
2019-03-12 13:10:26 -0400

    added jquery ui css

--- Layout.php
+++ Layout.php
@@ -395,6 +395,7 @@ $(document).ready(function() {
 			print("<script src=\"//ajax.googleapis.com/ajax/libs/jquery/{$version}/jquery.min.js\"></script>");
 		}
 		if ($uiVersion) {
+			print("<link rel=\"stylesheet\" href=\"https://ajax.googleapis.com/ajax/libs/jqueryui/{$uiVersion}/themes/smoothness/jquery-ui.css\">");
 			print("<script src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/{$uiVersion}/jquery-ui.min.js\"></script>");
 		}
 	}
