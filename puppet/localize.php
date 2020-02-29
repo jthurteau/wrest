6db4d7fd07f6a10497ffb3663051538ee68f643a
Troy Hurteau <troy.hurteau@gmail.com>
2020-02-29 10:05:13 -0500

    include the built in localize.php template

---
+++ puppet/localize.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', TRUE);
+define('INSTALL_PATH', '/var/www/application/saf');
+define('LIBRARY_PATH', '/var/www/application/library');
+define('APPLICATION_SUGGESTED_PORT', '8080');
+define('APPLICATION_FORCE_DEBUG', TRUE);
