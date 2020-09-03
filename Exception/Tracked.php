d7b1256dd011114b7bdd9231ee37aac1f4aed6ae
Troy Hurteau <troy.hurteau@gmail.com>
2020-09-03 15:55:20 -0400

    added debugging for a hard to track case in the Reflector, new exception types
    see: https://github.ncsu.edu/ncsu-libraries/roomres-ems/issues/244

---
+++ Exception/Tracked.php
@@ -0,0 +1,12 @@
+<?php //#SCOPE_OS_PUBLIC
+/*******************************************************************************
+#LIC_FULL
+
+@author Troy Hurteau <jthurtea@ncsu.edu>
+
+Exception for when an exception's "code" is a trackable ID
+
+*******************************************************************************/
+class Saf_Exception_Tracked extends Exception {
+
+}
